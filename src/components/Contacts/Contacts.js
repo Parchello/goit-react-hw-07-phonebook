43bfbaacd99fded8426a6246c8fc3741912f0ac7
Parchello <parchellos@gmail.com>
2023-12-25 00:54:52 +0200

    some styles

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -4,6 +4,14 @@ import { ContactDelButton } from './Contacts.styled';
 import { selectContacts, selectFilter } from 'Redux/selector';
 import { deleteContact, fetchContacts } from 'Redux/servise';
 import { useEffect } from 'react';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Container,
+} from '@mui/material';
 
 export const Contacts = () => {
   const value = useSelector(selectContacts);
@@ -18,25 +26,57 @@ export const Contacts = () => {
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+  function stringAvatar(name) {
+    const names = name.split(' ');
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: names.length > 1 ? `${names[0][0]}${names[1][0]}` : names[0][0],
+    };
+  }
+
   return (
-    <div>
-      {
-        <ul>
-          {visibleContacts.map(({ name, number, id }) => (
-            <li key={id}>
-              <p>
-                {name}: {number}
-              </p>
-              <ContactDelButton
-                type="button"
-                onClick={() => dispatch(deleteContact(id))}
-              >
-                delete
-              </ContactDelButton>
-            </li>
-          ))}
-        </ul>
-      }
-    </div>
+    <Container>
+      <List>
+        {visibleContacts.map(({ name, number, id }) => (
+          <ListItem key={id} alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar {...stringAvatar(name)} />
+            </ListItemAvatar>
+            <ListItemText primary={name} secondary={number} />
+            <ContactDelButton
+              variant="outlined"
+              color="error"
+              type="button"
+              onClick={() => dispatch(deleteContact(id))}
+            >
+              Delete
+            </ContactDelButton>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
   );
 };
--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -1,7 +1,14 @@
+import { Button } from '@mui/material';
 import styled from 'styled-components';
 
-export const ContactDelButton = styled.button`
+export const ContactDelButton = styled(Button)`
   &:hover {
     background-color: darkgray;
   }
+`;
+
+export const Container = styled.div`
+  display: 'flex';
+  flex-direction: 'row';
+  align-items: 'flex-start';
 `;
